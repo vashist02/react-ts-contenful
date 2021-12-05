170399115175e681f157fa592dfe2df5240af3a9
Divesh Heeramun <divesh.heeramun1@gmail.com>
2021-12-06 00:15:54 +0400

    fix(header): Remove redundant Storybook props (#13)

--- src/common/header/header.stories.tsx
+++ src/common/header/header.stories.tsx
@@ -6,9 +6,6 @@ import { Header } from './header';
 export default {
   title: 'Common/Header',
   component: Header,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as ComponentMeta<typeof Header>;
 
 const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
